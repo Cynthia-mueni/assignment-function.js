c4efdeadf43b6c2b44731733dfd0b701826ef9ad
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-04-03 12:27:26 +0300

    added changes

--- function.js
+++ function.js
@@ -1,35 +1,43 @@
-//1
-
-function sorting(){
-    const names=['cynthia','jane','faith','Ruby'];
-    console.log(names.sort())
-    console.log(log.reverse());
+//1.
+function reverseStringInArray(arr){
+  const reverseStrings = arr.map(str => [...str].reverse().join(''))
+  console.log(reverseStrings)
+  console.log(reverseStrings.sort())
 }
-//2
+reverseStringInArray(["Cynthia","Jane","Faith","Ruby"])
 
-function checkElement(){
-    let arr=arr.forEach(element =>{
-        if (element > 0) {
-            console.log("positive");
-          } else if (element < 0) {
-            console.log("negative");
-          } else {
-            console.log("zero");
-          }
-       });
-      }
-    
-      //3
-      let employee = [
-        { id: 1 },
-        { name: "Cynthia" },
-        { salary: [1000, 5000, 2000, 24000] },
-      ];
-        employee.sort(function(a, b) {
-        return a.salary - (b.salary);
-      });
-      employee.forEach((person) => console.log(employee.salary));
-     console.log(employee);
+
+//2
+function checkElement(array,index){
+const number = array[index]
+
+if(number>0){
+  return number + "is positive";
+}else if (number <0){
+  return number + "is negative";
+}else{
+  return number + "is zero";
+}
+}
+const numbers = [1,-2,0,3,-4];
+console.log(checkElement(numbers,3));
+
+
+//3 Given an array of objects, where each object represents an employee with an id, name, and salary property, 
+//write a function that returns a new array of employee objects sorted by their salary in ascending order.
+
+function sortBySalary(employees){
+return employees.sort((a, b) =>a.salary - b.salary )
+
+}
+const employees = [
+  {id:1,name:'Cynthia',salary:500},
+  {id:2,name:'Mueni',salary:1000},
+  {id:3,name:'Nthenya',salary:3000}
+]
+const myEmployees = sortBySalary(employees);
+console.log(myEmployees)
+   
 
 //4
 function multiply(numbers) {
